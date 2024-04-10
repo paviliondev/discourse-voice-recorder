c9e56c59c3fdbe3144565faeb52333d71337d250
Arkshine <github@arkshine.me>
2024-04-10 17:40:41 +0200

    DEV: Convert the modal to the new component-based API - 3
    - Convert controller to a glimmer component
    - Convert <button> to <DButton>
    - Move title to the <DModal>
    - Adjust toolbar.addButton action callback

--- javascripts/discourse/components/modal/audio-upload.js
+++ javascripts/discourse/components/modal/audio-upload.js
@@ -1,8 +1,8 @@
-import Controller from "@ember/controller";
+import { tracked } from "@glimmer/tracking";
+import Component from "@ember/component";
+import { action } from "@ember/object";
 import { equal, notEmpty } from "@ember/object/computed";
 import { uploadIcon } from "discourse/lib/uploads";
-import ModalFunctionality from "discourse/mixins/modal-functionality";
-import { default as discourseComputed } from "discourse-common/utils/decorators";
 
 function padStart(s, l, char) {
   let n = l - String(s).length;
@@ -12,70 +12,71 @@ function padStart(s, l, char) {
   return s;
 }
 
-export default Controller.extend(ModalFunctionality, {
-  state: "idle", // 'idle', 'recording', 'recording_start', 'playing', 'processing'
-  isRecording: equal("state", "recording"),
-  isRecordingStart: equal("state", "recording_start"),
-  isPlaying: equal("state", "playing"),
-  isProcessing: equal("state", "processing"),
-  isIdle: equal("state", "idle"),
-  hasRecording: notEmpty("_audioEl"),
+export default class AudioUpload extends Component {
+  @tracked state = "idle"; // 'idle', 'recording', 'recording_start', 'playing', 'processing'
 
-  @discourseComputed("state", "hasRecording")
-  disallowPlayback(state, hasRecording) {
-    return (state !== "idle" && state !== "playing") || !hasRecording;
-  },
+  @equal("state", "recording") isRecording;
+  @equal("state", "recording_start") isRecordingStart;
+  @equal("state", "playing") isPlaying;
+  @equal("state", "processing") isProcessing;
+  @equal("state", "idle") isIdle;
+  @notEmpty("_audioEl") hasRecording;
 
-  @discourseComputed("state")
-  disallowRecord(state) {
+  @tracked _audioEl = null;
+
+  _recorder = null;
+  _chunks = [];
+  _audioData = null;
+  _stream = null;
+
+  get disallowPlayback() {
     return (
-      state === "recording_start" || (state !== "idle" && state !== "recording")
+      (this.state !== "idle" && this.state !== "playing") || !this.hasRecording
     );
-  },
+  }
 
-  @discourseComputed("state")
-  disallowUpload(state) {
-    return state !== "idle";
-  },
+  get disallowRecord() {
+    return (
+      this.state === "recording_start" ||
+      (this.state !== "idle" && this.state !== "recording")
+    );
+  }
 
-  @discourseComputed("_audioData")
-  recordingSize(data) {
-    if (data) {
-      let bytes = data.size;
+  get disallowUpload() {
+    return this.state !== "idle";
+  }
+
+  get recordingSize() {
+    if (this._audioData) {
+      let bytes = this._audioData.size;
       return bytes < 1024
         ? bytes + " B"
         : Math.round((bytes * 10) / 1024) / 10 + " kB";
     }
     return "-";
-  },
+  }
 
-  _recorder: null,
-  _chunks: [],
-  _audioData: null,
-  _audioEl: null,
-  _stream: null,
-
-  @discourseComputed()
-  uploadIcon() {
+  get uploadIcon() {
     return uploadIcon(this.currentUser.staff, this.siteSettings);
-  },
+  }
 
   _clearRecording() {
     this._recorder = null;
-    this.set("_audioData", null);
+    this._audioData = null;
     if (this._audioEl) {
       this._audioEl.remove();
-      this.set("_audioEl", null);
+      this._audioEl = null;
     }
-  },
+  }
 
+  @action
   onShow() {
     this._clearRecording();
-  },
+  }
 
   onDataAvailable(e) {
     this._chunks.push(e.data);
-  },
+  }
 
   onStop() {
     let blob = new Blob(this._chunks, { type: this._recorder.mimeType });
@@ -93,48 +94,48 @@ export default Controller.extend(ModalFunctionality, {
       _audioData: blob,
       state: "idle",
     });
-  },
+  }
 
-  actions: {
-    uploadFile() {
-      if (!this._audioData) {
-        this.flash("You have to record something!", "error");
-        return;
-      }
-      this.appEvents.trigger(`composer:add-files`, [this._audioData]);
-      this.send("closeModal");
-    },
+  @action
+  uploadFile() {
+    if (!this._audioData) {
+      this.flash("You have to record something!", "error");
+      return;
+    }
+    this.appEvents.trigger(`composer:add-files`, [this._audioData]);
+    this.closeModal();
+  }
 
-    startStopRecording() {
-      if (this.state === "idle") {
-        this._clearRecording();
+  @action
+  startStopRecording() {
+    if (this.state === "idle") {
+      this._clearRecording();
 
-        navigator.mediaDevices
-          .getUserMedia({ audio: true })
-          .then((stream) => {
-            this._stream = stream;
-            this._recorder = new MediaRecorder(stream);
-            this._recorder.ondataavailable = this.onDataAvailable.bind(this);
-            this._recorder.onstop = this.onStop.bind(this);
-            this._recorder.start();
-            this.set("state", "recording_start");
-            setTimeout(() => {
-              this.set("state", "recording");
-            }, 1050);
-          })
-          .catch((err) => {
-            this.flash(
-              "An error occured. Did you enable voice recording in your browser?"
-            );
-            console.error(err);
-          });
-      } else if (this.state === "recording") {
-        this.set("state", "processing");
-        this._recorder.stop();
-        this._stream.getTracks().forEach((track) => {
-          track.stop();
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          this._stream = stream;
+          this._recorder = new MediaRecorder(stream);
+          this._recorder.ondataavailable = this.onDataAvailable.bind(this);
+          this._recorder.onstop = this.onStop.bind(this);
+          this._recorder.start();
+          this.state = "recording_start";
+          setTimeout(() => {
+            this.state = "recording";
+          }, 1050);
+        })
+        .catch((err) => {
+          this.flash(
+            "An error occured. Did you enable voice recording in your browser?"
+          );
+          console.error(err);
         });
-      }
-    },
-  },
-});
+    } else if (this.state === "recording") {
+      this.state = "processing";
+      this._recorder.stop();
+      this._stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+  }
+}
--- javascripts/discourse/initializers/audio-upload-initializer.js
+++ javascripts/discourse/initializers/audio-upload-initializer.js
@@ -1,24 +1,13 @@
-import { default as Composer } from "discourse/components/d-editor";
 import { withPluginApi } from "discourse/lib/plugin-api";
-import showModal from "discourse/lib/show-modal";
+import AudioUpload from "../components/modal/audio-upload";
 
 function initializePlugin(api) {
-  Composer.reopen({
-    actions: {
-      showAudioUploadModal: function () {
-        showModal("audio_upload", {
-          title: themePrefix("composer_audio_upload.title"),
-        });
-      },
-    },
-  });
-
   api.onToolbarCreate((toolbar) => {
     toolbar.addButton({
       id: "composer_audio_upload",
       group: "extras",
       icon: "microphone",
-      action: "showAudioUploadModal",
+      action: () => api.container.lookup("service:modal").show(AudioUpload),
       title: themePrefix("composer.composer_audio_upload_button_title"),
     });
   });
